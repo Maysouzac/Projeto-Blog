5b0e82a1733640c9c84816634ee8995eea335baa
Mayara <mayarasouzacosta@hotmail.com>
2023-07-21 14:26:23 -0300

    React

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './page/home/Home';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+   <Home />
+   </>
+  );
+}
+
+export default App;
---
+++ src/page/home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+import homeLogo from '../../assets/homeLogo.jpg'
+
+const Home = () => {
+  return (
+    <>
+        <h1 className='titulo'>Home</h1> 
+
+        <img src={homeLogo} className='img'  alt="Imagem Tela Inicial" />
+    </>
+
+  )
+}
+
+export default Home
